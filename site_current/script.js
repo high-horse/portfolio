39447000dca3e8173e2880921b195be3c8b71e89
AkhileshAdhikari <akhileshadhikaria1@gmail.com>
2024-04-23 22:25:56 +0545

    minor 2

--- site_current/script.js
+++ site_current/script.js
@@ -78,23 +78,23 @@ $(document).ready(function() {
     origin: "bottom"
   });
 
-//contact form to excel sheet
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzUSaaX3XmlE5m9YLOHOBrRuCh2Ohv49N9bs4bew7xPd1qlgpvXtnudDs5Xhp3jF-Fx/exec';
-const form = document.forms['submitToGoogleSheet']
-const msg = document.getElementById("msg")
+  //contact form to excel sheet
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbzUSaaX3XmlE5m9YLOHOBrRuCh2Ohv49N9bs4bew7xPd1qlgpvXtnudDs5Xhp3jF-Fx/exec';
+  const form = document.forms['submitToGoogleSheet']
+  const msg = document.getElementById("msg")
 
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-        .then(response => {
-            msg.innerHTML = "Message sent successfully"
-            setTimeout(function () {
-                msg.innerHTML = ""
-            }, 5000)
-            form.reset()
-        })
-        .catch(error => console.error('Error!', error.message))
-})
+  form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+          .then(response => {
+              msg.innerHTML = "Message sent successfully"
+              setTimeout(function () {
+                  msg.innerHTML = ""
+              }, 5000)
+              form.reset()
+          })
+          .catch(error => console.error('Error!', error.message))
+  })
   
 });
 
